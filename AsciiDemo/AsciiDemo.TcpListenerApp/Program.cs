eda690c897348d3eb56e326600dea6a112d44e7c
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-02-18 14:26:08 +0100

    tcp client-server

---
+++ AsciiDemo/AsciiDemo.TcpListenerApp/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AsciiDemo.TcpListenerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+
+                Int32 port = 502;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+                    stream.ReadTimeout = 10000;
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        //data = data.ToUpper();
+
+
+                        byte ACK = Convert.ToByte("06", 16);
+                        byte NAK = Convert.ToByte("15", 16);
+                        byte[] msg = new byte[1];
+
+
+                        //Reply back to caller
+                        Random random = new Random();
+                        var rand = random.Next(1, 3);
+
+                        if (rand > 1)
+                        {
+                            msg[0] = NAK;
+                        }
+                        else
+                            msg[0] = ACK;
+                       
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", GetDecodedData(msg));
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server?.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+        private static string GetDecodedData(byte[] data)
+        {
+            //Encoding.GetString() method converts an array of bytes into a string
+            return Encoding.ASCII.GetString(data);
+        }
+    }
+}
--- AsciiDemo/AsciiDemo.TestApp/Program.cs
+++ AsciiDemo/AsciiDemo.TestApp/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace AsciiDemo.TestApp
 {
@@ -12,10 +17,83 @@ namespace AsciiDemo.TestApp
         {
             //Test1();
             //Test2();
-
             var command = BuildCommand("ABC");
             PrintCommand(command);
 
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine();
+                Console.WriteLine($"sending command {i}...");
+                SendCommand(command);
+            }
+
+            Console.WriteLine($"Press any key to exit.");
+            Console.ReadLine();
+
+
+        }
+
+        private static void SendCommand(byte[] command)
+        {
+            using (TcpClient tcpClient = new TcpClient
+            {
+                ReceiveTimeout = 5000,
+                SendTimeout = 5000
+            })
+            {
+                try
+                {
+                    Int32 port = 502;
+                    IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                  
+                    tcpClient.Connect(localAddr, port);
+
+                    NetworkStream stream = tcpClient.GetStream();
+                    stream.ReadTimeout = 5000;
+
+                    var watch = Stopwatch.StartNew();
+
+                    var data = command.ToArray();
+                    stream.Write(data, 0, data.Length);//command sent
+
+                    var decodedCommand = GetDecodedData(command);
+                    Console.WriteLine($"command: {decodedCommand} was sent.");
+
+                    //reading response
+                    Byte[] response = new Byte[256];
+                    int length = 0;
+                    try
+                    {
+                        length = stream.Read(response, 0, response.Length);
+                    }
+                    catch (Exception ex)
+                    {
+                        // timeout happend
+                        Console.WriteLine(ex);
+                    }
+
+                    if (length > 0 && response[0] == 6) // Got ACk
+                    {
+                        Console.WriteLine("ACK recieved");
+                    }
+                    else if (length > 0 && response[0] == 21) // Got NAK
+                    {
+                        Console.WriteLine("NAK recieved");
+                    }
+
+                    Thread.Sleep(1000);
+
+                    stream.Close();
+                    watch.Stop();
+                    //Console.WriteLine(watch.ElapsedMilliseconds);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
         }
 
         public static void Test1()
@@ -114,13 +192,21 @@ namespace AsciiDemo.TestApp
 
             Console.WriteLine(BitConverter.ToString(command).Replace("-", "")); //outputs hex string to display on console
 
-            //Encoding.GetString() method converts an array of bytes into a string
-            var decodedData = Encoding.ASCII.GetString(command);
+           
+            var decodedData = GetDecodedData(command);
             Console.WriteLine(decodedData);
         }
 
 
         //Resources to look for more info
         //1.https://www.c-sharpcorner.com/article/c-sharp-string-to-byte-array/
+
+
+
+        private static string GetDecodedData(byte[] data)
+        {
+            //Encoding.GetString() method converts an array of bytes into a string
+            return Encoding.ASCII.GetString(data);
+        }
     }
 }
